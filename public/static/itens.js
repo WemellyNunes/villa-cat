b6bad927da22196517016d5c15830cba6504dce5
wemelly <wemelly@unifesspa.edu.br>
2023-03-15 17:36:18 -0300

    update: criação de pasta e arq json

---
+++ public/static/itens.js
@@ -0,0 +1,52 @@
+[
+    {
+      "id": "1",
+      "name": "Maya" ,
+      "image": "https://i.mlcdn.com.br/portaldalu/fotosconteudo/44281.jpg"
+    },
+    {
+        "id": "2",
+        "name": "Maya" ,
+        "image": "https://socientifica.com.br/wp-content/uploads/2020/12/cat-2068462_1280-scaled.jpg"
+      },
+      {
+        "id": "3",
+        "name": "Maya" ,
+        "image": "https://www.racoesreis.com.br/wordpress/wp-content/uploads/gato-dominar.jpg"
+      },
+      {
+        "id": "4",
+        "name": "Maya" ,
+        "image": "https://www.organnact.com.br/wp-content/uploads/2021/09/doencas-mais-comuns-em-gatos-no-inverno.jpg"
+      },
+      {
+        "id": "5",
+        "name": "Maya" ,
+        "image": "https://www.cafunepets.com.br/images/jw6al4qorxz1/4pm2ZCepjDCcPqJ28jY3Nk/e788fab686944ab7eec12ebf84b068b1/My5jdXJpb3NpZGFkZXMtc29icmUtZ2F0b3MucG5n/1280w-720h/8-curiosidades-sobre-gatos-pouco-conhecidas.jpg"
+      },
+      {
+        "id": "6",
+        "name": "Maya" ,
+        "image": "https://www.rbsdirect.com.br/imagesrc/25516126.jpg?w=700"
+      },
+      {
+        "id": "7",
+        "name": "Maya" ,
+        "image": "https://blog.cobasi.com.br/wp-content/uploads/2021/10/Curiosidades-sobre-gatos.png"
+      },
+      {
+        "id": "8",
+        "name": "Maya" ,
+        "image": "https://i.mlcdn.com.br/portaldalu/fotosconteudo/44281.jpg"
+      },
+      {
+        "id": "9",
+        "name": "Maya" ,
+        "image": "https://i.mlcdn.com.br/portaldalu/fotosconteudo/44281.jpg"
+      },
+      {
+        "id": "10",
+        "name": "Maya" ,
+        "image": "https://i.mlcdn.com.br/portaldalu/fotosconteudo/44281.jpg"
+      },
+]
