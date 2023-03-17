7dd00ca9295c85b8350d0777b1357c81e9503576
Wemelly <wemellyohanna@gmail.com>
2023-03-17 00:36:15 -0300

    fix: criação do carousel *ainda tem que finalizar

--- src/components/Carrosel-imgs/index.js
+++ src/components/Carrosel-imgs/index.js
@@ -1,36 +1,39 @@
-import { useSnapCarousel } from 'react-snap-carousel';
+import { useState, useEffect } from "react";
+import "./index.css"
 
-const BasicCarousel = () => {
-  const { scrollRef } = useSnapCarousel();
+function Carousel() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost300/static/itens.json')
+      .then((response) => response.json())
+      .then(setData);
+  }, [])
+
+  if (!data || !data.length) return null;
+
   return (
-    <ul
-      ref={scrollRef}
-      style={{
-        display: 'flex',
-        overflow: 'auto',
-        alignItems: 'center',
-        scrollSnapType: 'x mandatory'
-      }}
-    >
-      {Array.from({ length: 100 }).map((_, i) => (
-        <li
-          style={{
-            backgroundColor: 'aqua',
-            fontSize: '50px',
-            width: '250px',
-            height: '250px',
-            flexShrink: 0,
-            color: '#fff',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          Item {i}
-        </li>
-      ))}
-    </ul>
-  );
-};
+    <div className="container">
+      <div className="carousel">
 
-export default BasicCarousel;
+        {data.map((item) => {
+          const {id, name, image} = item;
+          return (
+
+            <div className="item" key={id}>
+              <div className="imagem">
+                <img src={image} alt="name" />
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+              </div>
+            </div>
+          );
+        }
+        )}
+      </div>
+    </div>
+  );
+}
+export default Carousel;
